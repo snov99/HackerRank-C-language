901d2573f1acd994cb035d83481be3b872fc4e1c
Snov LS <75370248+snov99@users.noreply.github.com>
2021-01-16 18:20:54 +0530

    Create PrintingPatternUsingLoops.c

---
+++ PrintingPatternUsingLoops.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    int n;
+    scanf("%d", &n);
+    int len = 2*n-1;
+  	for (int i=0;i<len-1;i++)
+      {
+          for (int j=0;j<=len-1;j++)
+          {
+                if (i==0 || i==len-1 || j==0 || j==len-1)
+                    printf("%d",n);
+                if ((i!=0 && j==1)||(i!=len-1 && j==1))
+                    printf("%d",n-1);
+                
+                printf(" ");
+          }
+          printf("\n");
+      }
+    return 0;
+}
