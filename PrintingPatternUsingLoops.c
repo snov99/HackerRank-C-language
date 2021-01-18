015b71332c18f0d8af886a2283a251ff26639148
Snov LS <75370248+snov99@users.noreply.github.com>
2021-01-18 18:09:29 +0530

    Update the file (final)

--- PrintingPatternUsingLoops.c
+++ PrintingPatternUsingLoops.c
@@ -8,18 +8,52 @@ int main()
     int n;
     scanf("%d", &n);
     int len = 2*n-1;
-  	for (int i=0;i<len-1;i++)
+    //first half of columns
+  	for (int i=1;i<=n;i++)
     {
-        for (int j=0;j<=len-1;j++)
+        //first half of raw
+        int temp1=1;
+        int pool[n-1];
+        for (int j=1;j<=n;j++)
         {
-            if (i==0 || i==len-1 || j==0 || j==len-1)
-                    printf("%d",n);
-            if ((i!=0 && j==1)||(i!=len-1 && j==1))
-                    printf("%d",n-1);
-                
-            printf(" ");
+            printf("%d ",n+1-temp1);
+            pool[n-1-j]=n+1-temp1;
+            if (temp1<i)
+                temp1++;
         }
-        printf("\n");
+        
+        //secound half of raw
+        for (int k=0;k<=n-2;k++)
+        {
+            printf("%d",pool[k]);
+            if (k !=n-2) 
+                printf(" ");
+        }
+        
+        printf("\n"); //new line for go to the next raw
+    }
+    //secound half of columns
+    for (int i=1;i<=n-1;i++)
+    {
+        //first half of raw
+        int temp1=1;
+        int pool[n-1];
+        for (int j=1;j<=n;j++)
+        {
+            printf("%d ",n+1-temp1);
+            pool[n-1-j]=n+1-temp1;
+            if (temp1<n-i)
+                temp1++;
+        }
+        
+        //secound half of raw
+        for (int k=0;k<=n-2;k++)
+        {
+            printf("%d",pool[k]);
+            if (k !=n-2) 
+                printf(" ");
+        }
+        printf("\n"); //new line for go to the next raw
     }
     return 0;
 }
